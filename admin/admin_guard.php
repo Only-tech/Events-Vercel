26109aea6b42d55688b1a6eedbf85b7ceaecbae2
Cédrick F <kcedrick185@gmail.com>
2025-07-26 21:05:29 +0200

    i'm done

--- admin/admin_guard.php
+++ admin/admin_guard.php
@@ -2,7 +2,7 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-require_once __DIR__ . '/includes/auth_functions.php';
+require_once __DIR__ . '/../includes/auth_functions.php';
 
 if (!isUserLoggedIn() || !isUserAdmin()) {
     $_SESSION['message'] = "Accès non autorisé. Vous devez être administrateur.";
